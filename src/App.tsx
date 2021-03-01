3511f644429f900d3066a958ed7c524c0e3ce4d7
Maiwan <natthapong@pirsquare.net>
2021-03-01 12:35:53 +0700

    add transition + animation

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Dark } from "./Dark";
 import { Presets } from "./Presets";
 import { Plugins } from "./Plugins";
 import { Grid } from './Grids'
+import { Transition } from './Transition'
 
 const browserHistory = createBrowserHistory();
 
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/plugins" component={Plugins} />
         <Route exact path="/presets" component={Presets} />
         <Route exact path='/grid' component={Grid} />
+        <Route exact path='/transition' component={Transition} />
       </Switch>
     </Router>
   );
